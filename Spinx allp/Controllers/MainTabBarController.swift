07528daac26df7e4e663d400732455a04b779f90
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-20 13:45:09 +0200

    MainTabBarController.swift refactor added new item for tabbar

--- Spinx allp/Controllers/MainTabBarController.swift
+++ Spinx allp/Controllers/MainTabBarController.swift
@@ -26,19 +26,22 @@ class MainTabBarController: UITabBarController {
     private func setupItems() {
         let mainVC = MainViewController()
         let menuVC = MenuViewController()
+        let aboutVC = AboutViewController()
         
         let menuNC = UINavigationController(rootViewController: menuVC)
         let mainNc = UINavigationController(rootViewController: mainVC)
         
-        setViewControllers([mainNc, menuNC], animated: true)
+        setViewControllers([mainNc, menuNC, aboutVC], animated: true)
         
         guard let items = tabBar.items else { return }
         
         items[0].title = "Home"
         items[1].title = "Menu"
+        items[2].title = "Info"
         
         items[0].image = UIImage(systemName: "house.fill")
         items[1].image = UIImage(systemName: "menucard.fill")
+        items[2].image = UIImage(systemName: "info.circle.fill")
         
         UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont(name: "Roboto-Bold", size: 12) as Any], for: .normal)
     }

08e33e9a559ff6fdb671ace8dce8d0601b16e13e
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-20 11:39:50 +0200

    MenuTableViewCell.swift added

---
+++ Spinx allp/Controllers/Menu/Cells/MenuTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MenuTableViewCell.swift
+//  Spinx allp
+//
+//  Created by BSergio on 18.04.2022.
+//
+
+import UIKit
+
+class MenuTableViewCell: UITableViewCell {
+
+    static let id = "MenuTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}

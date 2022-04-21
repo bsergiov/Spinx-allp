8ef3fcfbe638b6b0aad02420340b3e49921c8b16
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-21 17:44:33 +0200

    MenuCollectionViewCell.swift refactor content set for cell

--- Spinx allp/Controllers/Menu/Cells/MenuCollectionViewCell.swift
+++ Spinx allp/Controllers/Menu/Cells/MenuCollectionViewCell.swift
@@ -8,15 +8,53 @@
 import UIKit
 
 class MenuCollectionViewCell: UICollectionViewCell {
+    
     static let id = "MenuCollectionViewCell"
+    
+    lazy private var imageIcon: UIImageView = {
+       let image = UIImageView()
+        image.image = UIImage(systemName: "person")
+        image.contentMode = .scaleAspectFit
+        
+        image.layer.masksToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    func setupCell() {
+        selectedCell()
+        layer.cornerRadius = frame.height / 2
+        imageIcon.bounds = contentView.bounds
+        layer.masksToBounds = true
+        setupView()
+        setupConstraints()
+    }
     
     override var isSelected: Bool {
         didSet {
-            if self.isSelected {
-                backgroundColor = .blue
-            } else {
-                backgroundColor = .green
-            }
+            selectedCell()
         }
+    }
+    
+    // MARK: - Setup View
+    private func setupView() {
+        contentView.addSubview(imageIcon)
+    }
+    
+    // MARK: - Setup Constraints
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageIcon.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.8),
+            imageIcon.widthAnchor.constraint(equalTo: contentView.widthAnchor),
+            imageIcon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            imageIcon.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+    }
+    
+    // MARK: - Private Methodes
+    private func selectedCell() {
+        backgroundColor = isSelected ? .green : .blue
+        layer.borderColor = isSelected ? UIColor.white.cgColor : UIColor.black.cgColor
+        layer.borderWidth = 4
     }
 }
